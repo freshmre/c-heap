8136e41e67029ff53feae308e85409cbbabf63cf
freshmre <adelrich@spoiledmre.com>
2022-01-07 00:50:15 +0100

    Cleanup

--- main.c
+++ main.c
@@ -1,42 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <strings.h>
 #include "heap.h"
 #include "tokenizer.h"
 
 #define SIZE 1024
 
-void test_heap(void);
 int mystrcmp(void *a, void *b);
 
 int mystrcmp(void *a, void *b)
 {
-    // int lendiff = strlen(a) - strlen(b);
-    // if (lendiff != 0)
-    //     return lendiff;
-    // else
-        return strcasecmp(a, b);
+    return strcasecmp(a, b);
 }
-
-void test_heap(void)
-{
-    char *buf[] = {"aa", "ab", "bc", "zz", "ak", "lm"};
-
-    heap *h = init_heap(mystrcmp);
-    for (size_t i = 0; i < sizeof(buf) / sizeof(*buf); i++)
-        push_heap(h, buf[i], strlen(buf[i]) + 1);
-
-    char *sOut;
-    for (int i = 0; !is_empty(h); i++)
-    {
-        sOut = pop_heap(h);
-        printf("%s\n", sOut);
-        free(sOut);
-    }
-    free_heap(h);
-}
-
 
 int main(int argc, char *argv[])
 {
@@ -49,9 +25,7 @@ int main(int argc, char *argv[])
     char s[SIZE];
     heap *h = init_heap(mystrcmp);
     while (getword(fp, s, SIZE) != EOF)
-    {
         push_heap(h, s, strlen(s) + 1);
-    }
 
     char *sOut;
     for (int i = 0; !is_empty(h); i++)
@@ -60,7 +34,7 @@ int main(int argc, char *argv[])
         printf("%s\n", sOut);
         free(sOut);
     }
+
     free_heap(h);
     fclose(fp);
-    // test_heap();
 }
